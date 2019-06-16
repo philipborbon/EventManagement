6f0e5787886b3f5b34a318d6dadc25de024f6ce1
Philip Borbon <philipjohnborbon@gmail.com>
2019-06-17 06:54:39 +0800

    Remove required auth in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,7 @@ use EventManagement\Activity;
 use EventManagement\Event;
 
 class HomeController extends Controller
-{
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
+{   
     /**
      * Show the application dashboard.
      *
