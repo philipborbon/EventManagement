8ee172b80322152ba4c0f7ed9c31708190571497
Philip Borbon <philipjohnborbon@gmail.com>
2019-11-26 06:14:51 +0800

    Fix double h tag

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col-8"><h1><h1>Events</h1></h1></div>
+    <div class="col-8"><h1>Events</h1></div>
     <div class="col-4 text-right"><a href="{{action('EventController@print', ['keyword' => $keyword, 'start' => $start, 'end' => $end])}}" class="btn btn-primary" target="_blank">Print</a></div>
   </div>
 
